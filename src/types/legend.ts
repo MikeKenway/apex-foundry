c2ed61f79b48d583808d47236f583c602f6919dd
Mike Kenway <mike@mikekenway.com>
2025-06-14 19:19:31 -0400

    fixing compile errors

--- src/types/legend.ts
+++ src/types/legend.ts
@@ -1,51 +1,56 @@
 export type AbilityDetails = {
-  name: string
-  description: string | null
-}
+  name: string;
+  description: string | null;
+};
 
 export type Abilities = {
-  tactical: AbilityDetails
-  passive: AbilityDetails
-  ultimate: AbilityDetails
-}
+  tactical: AbilityDetails;
+  passive: AbilityDetails;
+  ultimate: AbilityDetails;
+};
 
 export type Relative = {
-  name: string | null
-  status: string | null
-  notes?: string
-}
+  name: string | null;
+  status: string | null;
+  notes?: string;
+};
 
 export type Family = {
-  [key: string]: Relative | Relative[] | null
-} | null
+  mother?: Relative | null;
+  father?: Relative | null;
+  brothers?: Relative[] | null;
+  sisters?: Relative[] | null;
+  others?: Relative[] | null;
+} | null;
 
 export type Backstory = {
-  early_life?: string
-  career_before_games?: string
-  joining_apex?: string
-} | null
+  early_life?: string;
+  career_before_games?: string;
+  joining_apex?: string;
+} | null;
 
 export type Personality = {
-  traits?: string[]
-  insecurities?: string
-  hobbies?: string[]
-} | null
+  traits?: string[];
+  insecurities?: string;
+  hobbies?: string[];
+} | null;
 
 export type Legend = {
-  name: string
-  slug: string
-  real_name: string
-  age: string | null
-  homeworld: string | null
-  class: string
-  voice_actor: string | null
-  appearance_season: string
-  title: string
-  abilities: Abilities
-  family: Family
-  backstory: Backstory
-  notable_quotes: string[] | null
-  fun_facts: string[] | null
-  image: string
-  sources: string[] | null
-}
+  name: string;
+  slug: string;
+  real_name: string;
+  age: string | null;
+  homeworld: string | null;
+  class: string;
+  voice_actor: string | null;
+  appearance_season: string;
+  title: string;
+  image: string;
+  abilities: Abilities;
+  family: Family;
+  backstory: Backstory;
+  personality?: Personality;
+  notable_quotes?: string[];
+  fun_facts?: string[];
+  sources?: string[];
+};
